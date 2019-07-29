a02b93e0a141a69c149c164851761f5cd6f47d6d
Raj Kumar Bharati <bharatir34@gmail.com>
2019-07-30 00:28:27 +0545

    gen changes

--- miranda.WebCore/Class1.cs
+++ miranda.WebCore/Class1.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using Microsoft.AspNetCore.Http;
+using System.Globalization;
+using System.Threading.Tasks;
 
 namespace miranda.WebCore
 {
-    public class Class1
+    public class CustRqHandler
     {
+        private readonly HttpRequest _next;
+        
     }
 }
